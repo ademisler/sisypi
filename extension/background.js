58263d69dc28641864ac5261c439600761b52b21
Adem Isler <75990511+ademisler@users.noreply.github.com>
2025-07-05 01:40:18 +0200

    Implement Sisypi extension

--- extension/background.js
+++ extension/background.js
@@ -1,1 +1,37 @@
 // Handles tab switching and background messaging
+
+importScripts('storage.js', 'runner.js');
+
+let currentRecording = null; // {name, steps}
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  if (msg.type === 'startRecording') {
+    currentRecording = { name: msg.name, steps: [] };
+    if (sender.tab) {
+      chrome.tabs.sendMessage(sender.tab.id, { type: 'startRecording' });
+    }
+  } else if (msg.type === 'stopRecording') {
+    if (sender.tab) {
+      chrome.tabs.sendMessage(sender.tab.id, { type: 'stopRecording' });
+    }
+    if (currentRecording) {
+      saveScenario(currentRecording.name, currentRecording.steps);
+    }
+    currentRecording = null;
+  } else if (msg.type === 'recordStep') {
+    if (currentRecording) {
+      currentRecording.steps.push(msg.step);
+    }
+  } else if (msg.type === 'runScenario') {
+    loadScenario(msg.name, (steps) => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        if (tabs[0]) {
+          runScenarioSteps(tabs[0].id, steps);
+        }
+      });
+    });
+  } else if (msg.type === 'getRecordingStatus') {
+    sendResponse({ recording: !!currentRecording });
+  }
+  return true;
+});
--- extension/recorder.js
+++ extension/recorder.js
@@ -1,1 +1,17 @@
 // Records user actions as structured blocks
+
+class Recorder {
+  constructor() {
+    this.steps = [];
+  }
+
+  add(step) {
+    this.steps.push(step);
+  }
+
+  clear() {
+    this.steps = [];
+  }
+}
+
+self.Recorder = Recorder;
